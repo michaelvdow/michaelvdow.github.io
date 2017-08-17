ca15b3929a399405a124998d1b4314dbf5870162
Michael Dow <michaelvdow@gmail.com>
2017-08-17 02:21:12 -0500

    JS File

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function uiucPage(){
+    window.open('https://cs.illinois.edu/', '_blank');
+}
+
+function imsaPage(){
+    window.open('http://www.imsa.edu', '_blank');
+}
+
+function researchPage(){
+    window.open('http://stackoverflow.com/', '_blank');
+}
+
+function roboticsPage(){
+    window.open('https://www.facebook.com/TitanRobotics2022/', '_blank');
+}
