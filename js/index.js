1a218c6ea223f8aeb71134b1d8a8fa060bae9ae0
Michael Dow <michaelvdow@gmail.com>
2017-08-17 21:19:01 -0500

    First version completed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,66 @@
+$(document).ready(function(e) {
+    
+    $("#homeTab").on('click', function(e){
+        e.preventDefault();        
+        $('html, body').animate({
+            scrollTop: $("#Home").offset().top - 50
+        }, 500, function(){
+        });
+        $('.nav li').removeClass('active');
+        $('#homeTab').addClass('active');
+    });
+    
+    $("#aboutMeTab").on('click', function(e){
+        e.preventDefault();        
+        $('html, body').animate({
+            scrollTop: $("#AboutMe").offset().top - 50
+        }, 500, function(){
+        });
+        $('.nav li').removeClass('active');
+        $('#aboutMeTab').addClass('active');
+    });
+    
+    $("#projectsTab").on('click', function(e){
+        e.preventDefault();        
+        $('html, body').animate({
+            scrollTop: $("#Projects").offset().top - 50
+        }, 500, function(){
+        });
+        $('.nav li').removeClass('active');
+        $('#projectsTab').addClass('active');
+    });
+    
+    $("#resumeTab").on('click', function(e){
+        e.preventDefault();        
+        $('html, body').animate({
+            scrollTop: $("#Resume").offset().top - 50
+        }, 500, function(){
+        });
+        $('.nav li').removeClass('active');
+        $('#resumeTab').addClass('active');
+    });
+    
+    $("#contactTab").on('click', function(e){
+        e.preventDefault();        
+        $('html, body').animate({
+            scrollTop: $("#Contact").offset().top - 50
+        }, 500, function(){
+        });
+        $('.nav li').removeClass('active');
+        $('#contactTab').addClass('active');
+    });
+    
+    
+    
+});
+
+
+function scrollTo(divName){
+    $('html,body').animate({
+        scrollTop: $(divName).offset().top
+    }, 'slow');
+}
+
 function uiucPage(){
     window.open('https://cs.illinois.edu/', '_blank');
 }
@@ -7,9 +70,10 @@ function imsaPage(){
 }
 
 function researchPage(){
-    window.open('http://stackoverflow.com/', '_blank');
+    window.open('http://www.pac-lab.org/', '_blank');
 }
 
 function roboticsPage(){
     window.open('https://www.facebook.com/TitanRobotics2022/', '_blank');
 }
+
